86c1ca31abbd99593ddc5031871cebc7ec200dca
nukrig <n_gegelashvili@cu.edu.ge>
2023-01-05 17:55:13 +0400

    add

---
+++ main.js
@@ -0,0 +1,38 @@
+const form=document.getElementById('form')
+const text=document.querySelector('input[name="text"]')
+const char=document.querySelector('input[name="char"]')
+const btn=document.getElementById('button')
+const li=document.getElementById('li')
+
+
+function count(text,char){
+    let result=0
+    for(let i=0;i<text.length; i++){
+        if(text[i]===char){
+            result=result+1
+        }
+    }
+    li.textContent= result
+    li.style.marginTop='15px'
+    li.style.fontSize='30px'
+    if(result===0){
+        li.style.color='red'
+    }else if(result<=10){
+        li.style.color='yellow'
+    }else{
+        li.style.color='green'
+    }
+}
+
+btn.addEventListener('click', ()=>{
+    let inputValue = text.value
+    let charValue=char.value
+    if(!inputValue || !charValue){
+        alert('Empty Value Provided')
+        return
+    }
+    
+    count(inputValue,charValue)
+    text.value=''
+    char.value=''
+})
